d0277da47710b76a53849224c40ef0229f10d94a
Michael JasonSmith <mpj17@onlinegroups.net>
2008-08-14 02:48:38 +0000

    Merge from Can Post code.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSGroupMember/trunk%404067

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
+import usercanpostcontentprovider
 from AccessControl import ModuleSecurityInfo
 from AccessControl import allow_class
 
-xwfutils_security = ModuleSecurityInfo('Products.GSGroupMember.groupmembership')
-xwfutils_security.declarePublic('get_group_users')
-xwfutils_security.declarePublic('get_unverified_group_users')
+groupmembership_security = ModuleSecurityInfo('Products.GSGroupMember.groupmembership')
+groupmembership_security.declarePublic('get_group_users')
+groupmembership_security.declarePublic('get_unverified_group_users')
+groupmembership_security.declarePublic('join_group')
+groupmembership_security.declarePublic('user_member_of_group')
 
