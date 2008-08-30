5ee4356d88175a739ae286bbba8f8eefb03baf57
Richard Waid <richard@iopen.net>
2008-08-30 03:17:47 +0000

    little more debugging
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSGroupMember/trunk%404150

--- usercanpost.py
+++ usercanpost.py
@@ -17,6 +17,9 @@ from Products.GSSearch.queries import MessageQuery
 from Products.GSProfile import interfaces as profileinterfaces
 from interfaces import IGSPostingUser
 
+import logging
+log = logging.getLogger("GSGroupMember.usercanpost")
+
 class GSGroupMemberPostingInfo(object):
 
     adapts(IGSGroupFolder, IGSUserInfo)
@@ -183,10 +186,12 @@ class GSGroupMemberPostingInfo(object):
             retval = False
             self.__status = u'participation coach'
             self.__statusNum = self.__statusNum + 0
+            log.info("User is participation coach")
         elif user_admin_of_group(self.userInfo, self.groupInfo):
             retval = False
             self.__status = u'administrator of'
             self.__statusNum = self.__statusNum + 0
+            log.info("User is administrator")
         else:
             # The user is not the participation coach or the administrator
             # of the group
@@ -195,6 +200,7 @@ class GSGroupMemberPostingInfo(object):
             uid = self.userInfo.id
             limit = self.mailingList.getValueFor('senderlimit')
             interval = self.mailingList.getValueFor('senderinterval')
+            log.info("Group limit set to %s messages in %s seconds" % (limit, interval))
             td = timedelta(seconds=interval)
             now = datetime.now(pytz.utc)
             earlyDate = now - td
