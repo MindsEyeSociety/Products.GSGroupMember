301c538c92f0cb4042a7ba2bbbe679184a0d9bda
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-30 14:04:19 -0700

    Adding a copyright and license.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -10,10 +10,17 @@ setup(name='Products.GSGroupMember',
       description="",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
         "Programming Language :: Python",
-        ],
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Michael JasonSmith',
       author_email='mpj17@onlinegroups.net',
