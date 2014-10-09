70aefe698f2fe2285865acbb1597ce0350943506
Michael JasonSmith <mpj17@onlinegroups.net>
2014-10-09 16:10:22 -0700

    Updating the product metadata

--- README.rst
+++ README.rst
@@ -1,4 +1,39 @@
+==========================
+``Products.GSGroupMember``
+==========================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The old group-membership code for GroupServer
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Michael JasonSmith`_
+:Contact: Michael JasonSmith <mpj17@onlinegroups.net>
+:Date: 2014-10-09
+:Organization: `GroupServer.org`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 4.0 International License`_
+  by `OnlineGroups.Net`_.
+
 Introduction
 ============
+
+The code for managing group membership in GroupServer_ groups
+used to be defined in this product. Over time the code in this
+product is being farmed out to the ``gs.group.member.*`` products
+in general, and `gs.group.member.base`_ in particular.
+
+Resources
+=========
+
+- Code repository: https://github.com/groupserver/Products.GSGroupMember
+- Questions and comments to http://groupserver.org/groups/development
+- Report bugs at https://redmine.iopen.net/projects/groupserver
+
+.. _GroupServer: http://groupserver.org/
+.. _GroupServer.org: http://groupserver.org/
+.. _OnlineGroups.Net: https://onlinegroups.net
+.. _Michael JasonSmith: http://groupserver.org/p/mpj17
+.. _Creative Commons Attribution-Share Alike 4.0 International License:
+    http://creativecommons.org/licenses/by-sa/4.0/
+.. _gs.group.member.base: https://github.com/groupserver/gs.group.member.base
 
 
--- setup.py
+++ setup.py
@@ -1,30 +1,51 @@
 # -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2009, 2010, 2011, 2012, 2013, 2014 OnlineGroups.net and
+# Contributors.
+#
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='Products.GSGroupMember',
       version=version,
-      description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      description="The old group-membership code for GroupServer",
+      long_description=long_description,
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        "Environment :: Web Environment",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux"
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
       ],
       keywords='',
       author='Michael JasonSmith',
       author_email='mpj17@onlinegroups.net',
-      url='http://groupserver.org',
+      url='https://github.com/groupserver/Products.GSGroupMember',
       license='ZPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
